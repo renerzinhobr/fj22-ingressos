e012943407b789c77881050fa849fe82a34a5ae1
Rener <rener.lemos@live.com>
2020-11-11 22:48:28 -0300

    Mensagem Explicando o trabalho que foi feito

---
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -0,0 +1,15 @@
+package br.com.caelum.ingresso.model;
+
+import java.time.LocalTime;
+
+public class Sessao	{
+	private	Integer	id;
+	private	LocalTime	horario;
+	private	Sala	sala;
+	private	Filme	filme;
+	public	Sessao(LocalTime	horario,	Filme	filme,	Sala	sala) {
+					this.horario	=	horario;
+					this.filme	=	filme;
+					this.sala	=	sala;
+	}
+}
